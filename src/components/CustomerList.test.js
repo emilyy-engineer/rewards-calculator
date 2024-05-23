b1f2ad128dbf5ef0c3a733eb1d157453b512e1e9
emilyy-engineer <emilyy.engineer@gmail.com>
2024-05-23 11:19:17 -0500

    write tests for CustomerList

---
+++ src/components/CustomerList.test.js
@@ -0,0 +1,42 @@
+import React,{ act }  from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import CustomerList from "./CustomerList";
+import { fetchCustomers } from "../api/fetchData";
+import { BrowserRouter as Router } from "react-router-dom";
+
+jest.mock("../api/fetchData.js")
+
+describe("CustomerList Component", () => {
+
+  beforeEach(() => {
+    fetchCustomers.mockClear();
+  })
+
+  it("fetches and displays customers", async ()=> {
+    const mockCustomers = [
+      { customerId: 1, customerName: "John Doe" },
+      { customerId: 2, customerName: "Jane Smith" }
+    ];
+    fetchCustomers.mockResolvedValueOnce(mockCustomers);
+    render(<CustomerList/>)
+
+
+
+    expect(screen.getByText("Customer List")).toBeInTheDocument();
+    // await waitFor(() => {
+    //   expect(screen.getByText('John Doe')).toBeInTheDocument();
+      
+    // });
+    
+  })
+
+  it("handles empty customer list", async () => {
+    fetchCustomers.mockResolvedValueOnce([]);
+    render(<CustomerList />);
+    expect(screen.getByText("Customer List")).toBeInTheDocument();
+    // await waitFor(() => {
+    //   expect(screen.queryByRole('listitem')).not.toBeInTheDocument();
+    // })
+  })
+})
